4bcaedf82c3e39e0cd77af7a6c92879e8e8f5b11
JizongL <2015rpro@gmail.com>
2019-02-24 09:56:11 -0500

    added footer and fix name

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 // Add Event Listeners here:
 'use strict';
 const PEOPLE=[
-  {id: 1, name: 'Eddie_Bueno', img: 'assets/images/01_Eddie_Bueno.png'},
-  {id: 2, name: 'James_Scherernges', img: 'assets/images/02_James_Scherer.png'},
+  {id: 1, name: 'Eddie Bueno', img: 'assets/images/01_Eddie_Bueno.png'},
+  {id: 2, name: 'James Scherernges', img: 'assets/images/02_James_Scherer.png'},
   {id: 3, name: 'Jody', img: 'assets/images/03_Jody.png'},
-  {id: 4, name: 'David_Baker', img: 'assets/images/04_David_Baker.png'},
-  {id: 5, name: 'Laural_Butler', img: 'assets/images/05_Laural_Butler.png'},
-  {id: 6, name: 'David_Bolin', img: 'assets/images/06_David_Bolin.png'},
-  {id: 7, name: 'David_Liang', img: 'assets/images/07_David_Liang.png'},
-  {id: 8, name: 'Farah_Gustafson', img: 'assets/images/08_Farah_Gustafson.png'},
-  {id: 9, name: 'Joel_Corey', img: 'assets/images/09_Joel_Corey.png'},
-  {id: 10, name: 'Enrique_Montemayor', img: 'assets/images/10_Enrique_Montemayor.png'},
-  {id: 11, name: 'Jay_Lee', img: 'assets/images/11_Jay_Lee.png'},
-  {id: 12, name: 'Rich_Greenhill', img: 'assets/images/12_Rich_Greenhill.png'}
+  {id: 4, name: 'David Baker', img: 'assets/images/04_David_Baker.png'},
+  {id: 5, name: 'Laural Butler', img: 'assets/images/05_Laural_Butler.png'},
+  {id: 6, name: 'David Bolin', img: 'assets/images/06_David_Bolin.png'},
+  {id: 7, name: 'David Liang', img: 'assets/images/07_David_Liang.png'},
+  {id: 8, name: 'Farah Gustafson', img: 'assets/images/08_Farah_Gustafson.png'},
+  {id: 9, name: 'Joel Corey', img: 'assets/images/09_Joel_Corey.png'},
+  {id: 10, name: 'Enrique Montemayor', img: 'assets/images/10_Enrique_Montemayor.png'},
+  {id: 11, name: 'Jay Lee', img: 'assets/images/11_Jay_Lee.png'},
+  {id: 12, name: 'Rich Greenhill', img: 'assets/images/12_Rich_Greenhill.png'}
   ];
 
 
@@ -54,7 +54,8 @@ function generateGrid(n){
   let gridElementString = [];
   for (let i = 0;i<n;i++){
     gridElementString.push(`<div class="col-3">
-  <div data-item-id = ${array.pop()} class="box"><img src=''>for protrait</div>
+  <div data-item-id = ${array.pop()} class="box"><img src='' alt=''>
+  <div><span id='name'></span></div></div>
 </div>`);
   }
   return gridElementString.join('');
@@ -72,7 +73,10 @@ function drawGrids(){
       const itemIndex = $(this).data('item-id');
       const portrait_path = PEOPLE[itemIndex].img;
       console.log(portrait_path);
+      const name = PEOPLE[itemIndex].name;
       $(this).find('img').attr('src',portrait_path);
+      $(this).find('img').attr('alt',name);
+      $(this).find('#name').text(name);
 
    });
   });
